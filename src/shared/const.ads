45d6764cc49e5f4a2e54953b7b6d2874e95ae9c6
Bryan Guano <bryan.guano@epn.edu.ec>
2026-02-01 13:38:18 -0500

    feat: add constants for application limits in Length package

---
+++ src/shared/const.ads
@@ -0,0 +1,10 @@
+package Lenght is
+   pragma Pure;
+
+   MAX_ID : constant := 10;
+   MAX_TEXTO_CORTO   : constant := 50;
+   MAX_TEXTO_LARGO : constant := 150;
+   MAX_TELEFONO    : constant := 15;
+   MAX_DINERO    : constant := 10;
+
+end Length;
